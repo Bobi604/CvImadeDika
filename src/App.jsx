a42561a2ee12faae48f982a8aa86d80a61febbcc
Bobi604 <bobi.dika02@gmail.com>
2025-09-02 10:08:53 +0800

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,10 +22,10 @@ function App() {
             animate={{ opacity: 1, scale: 1 }}
             exit={{ opacity: 0, scale: 1.1 }}
             transition={{ duration: 0.6 }}
-            className="text-center text-white"
+            className="text-center text-white z-10"
           >
             <motion.h1
-              className="text-5xl font-bold text-gold mb-4"
+              className="text-5xl font-bold text-gold mb-4 z-10"
               initial={{ y: -50, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
               transition={{ delay: 0.3, duration: 0.7 }}
@@ -34,20 +34,21 @@ function App() {
             </motion.h1>
 
             <motion.p
-              className="text-lg text-gray-300 mb-8"
+              className="text-lg text-gray-300 mb-8 z-10"
               initial={{ y: 30, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
               transition={{ delay: 0.6, duration: 0.7 }}
             >
-              CV <span className="text-gold font-semibold">I MADE DIKA</span> -
-              Web Developer
+              CV{" "}
+              <span className="text-gold font-semibold z-10">I MADE DIKA</span>{" "}
+              - Web Developer
             </motion.p>
 
             <motion.button
               onClick={() => setShowCV(true)}
               whileHover={{ scale: 1.1 }}
               whileTap={{ scale: 0.95 }}
-              className="px-8 py-3 bg-gold text-dark font-semibold rounded-xl shadow-lg hover:bg-zinc-700 transition"
+              className="px-8 py-3 bg-gold text-dark font-semibold rounded-xl shadow-lg hover:bg-zinc-700 transition z-10"
             >
               Lihat CV
             </motion.button>
